1d0e0b05a2f78868199a9cbcb506b4099b2ff6f8
romaisasuliman <82890280+romaisasuliman@users.noreply.github.com>
2021-04-20 17:50:06 +0300

    ppp

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pattern {
+    class Program {
+        static void Main(string[] args) {
+            for (int row = 1; row <= 10; row++) {
+                for (int col = 1; col <= 10; col++) {
+                    int mlply = row * col;
+                    if ((row == 1) || (col == 1) || ((mlply > 20) && (mlply < 60))) {
+                        Console.Write($"{mlply,4}" );
+                    } else {
+                        Console.Write("....");
+                    }
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
